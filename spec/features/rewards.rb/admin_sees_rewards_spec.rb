d5fade05bcccc2226869ee80e2069ace4a2df066
hmesander <haleymesander@gmail.com>
2018-04-02 15:44:07 -0600

    Adds test for default user not to see button for creating a new reward

--- spec/features/rewards.rb/admin_sees_rewards_spec.rb
+++ spec/features/rewards.rb/admin_sees_rewards_spec.rb
@@ -11,4 +11,16 @@ describe 'User visits rewards index page' do
       expect(page).to have_content('Add a Reward')
     end
   end
+
+  context 'as default user' do
+    it 'does not allow default user to see button to create a new reward' do
+      user = User.create!(username: 'hmesander', password: 'test', role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit admin_rewards_path
+
+      expect(page).to_not have_content('Add a Reward')
+      expect(page).to have_content('Rewards')
+    end
+  end
 end
