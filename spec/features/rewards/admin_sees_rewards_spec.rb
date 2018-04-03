1e49a8a7598bc30d05accec8959e803b9cf28e06
hmesander <haleymesander@gmail.com>
2018-04-03 15:20:07 -0600

    Adds test that guest can see a list of rewards

--- spec/features/rewards/admin_sees_rewards_spec.rb
+++ spec/features/rewards/admin_sees_rewards_spec.rb
@@ -1,25 +1,19 @@
 require 'rails_helper'
 
-describe 'User visits rewards index page' do
-  context 'as admin' do
-    it 'allows admin to see a button to create a new reward' do
+describe 'Admin' do
+  describe 'visits rewards index' do
+    it 'they see rewards and buttons to create, update and delete rewards' do
       admin = User.create!(username: 'hmesander', password: 'test', role: 1)
+      reward = Reward.create(title: 'PhoneTime', value: 30, description: 'Awesome!', image: 'public/phone.jpeg')
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       visit admin_rewards_path
+
+      expect(page).to have_content('PhoneTime')
       expect(page).to have_content('Add a Reward')
-    end
-  end
-
-  context 'as default user' do
-    it 'does not allow default user to see button to create a new reward' do
-      user = User.create!(username: 'hmesander', password: 'test', role: 0)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit admin_rewards_path
-
-      expect(page).to_not have_content('Add a Reward')
+      expect(page).to have_content('Update Reward')
+      expect(page).to have_content('Delete Reward')
     end
   end
 end
