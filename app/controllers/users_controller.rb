ad93ecaecdf91e4ba3a6f739ca7ff49d57d40930
hmesander <haleymesander@gmail.com>
2018-04-04 17:28:11 -0600

    Adds ability of admin to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,12 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+      if current_admin?
+        redirect_to admin_users_path
+      else
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      end
     else
       render :new
     end
