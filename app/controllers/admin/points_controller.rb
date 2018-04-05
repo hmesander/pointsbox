17178fdc72c7e5a5c23d1623700da315dbcf7611
hmesander <haleymesander@gmail.com>
2018-04-05 02:03:34 -0600

    Fixes bug in points controller

--- app/controllers/admin/points_controller.rb
+++ app/controllers/admin/points_controller.rb
@@ -11,7 +11,10 @@ class Admin::PointsController < Admin::BaseController
   end
 
   def destroy
-    @point = User.find(params[:user_id]).points.last.destroy
-    redirect_to admin_user_path(@point.user)
+    @user = User.find(params[:user_id])
+    if @user.points.count >= 1
+      @point = @user.points.last.destroy
+    end
+    redirect_to admin_user_path(@user)
   end
 end
